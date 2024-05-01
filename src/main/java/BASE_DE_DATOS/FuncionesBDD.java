43d6c014a4b65923600d47866cf955f39eb4f4da
PB-421 <pablo.borderas@gmail.com>
2024-05-01 14:34:48 +0200

    Ahora en la base datos se añaden los 10  hoteles

--- src/main/java/BASE_DE_DATOS/FuncionesBDD.java
+++ src/main/java/BASE_DE_DATOS/FuncionesBDD.java
@@ -39,7 +39,12 @@ public class FuncionesBDD {
     public static void intsertBD(Connection BD){
         try{
             Statement statement = BD.createStatement();
-            String sqlquery="INSERT INTO hoteles(idHotel,Nombre,Ciudad)VALUES('"+idHotel+"','"+nombreHotel+"','"+ciudad+"')";
+            String sqlquery = "INSERT INTO hoteles (idHotel, Nombre, Ciudad) " +
+                    "SELECT * FROM (SELECT '" + idHotel + "', '" + nombreHotel + "', '" + ciudad + "') AS tmp " +
+                    "WHERE NOT EXISTS (" +
+                    "    SELECT idHotel FROM hoteles WHERE idHotel = '" + idHotel + "'" +
+                    ") LIMIT 1;";
+            //String sqlquery="INSERT INTO hoteles(idHotel,Nombre,Ciudad)VALUES('"+idHotel+"','"+nombreHotel+"','"+ciudad+"')";
             statement.executeUpdate(sqlquery);
             System.out.println("Datos insertados");
         }catch (SQLException e){
