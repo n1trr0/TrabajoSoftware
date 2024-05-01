f9e1832b6784ad06f2109bcfdb18723a12d6a929
PB-421 <pablo.borderas@gmail.com>
2024-05-01 14:50:29 +0200

    Funcion que devuelve un ArrayList con los nombres de los hoteles en la base de datos (Nueva clase FuncionesHoteles disponible en BBDD)

---
+++ src/main/java/BBDD/FuncionesHoteles.java
@@ -0,0 +1,39 @@
+package BBDD;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class FuncionesHoteles {
+
+    public static ArrayList<String> conseguirNombreHoteles(Connection BD){
+        ArrayList<String> Hoteles = new ArrayList<>();
+        try {
+            //creo un statement
+            Statement statement = BD.createStatement();
+            // Seleciono una tabla de la base de datos
+            String sqlQuery = "SELECT * FROM hoteles";//puedo poner columnas individuales tambien
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            if(!resultSet.next()){
+                System.out.println("La tabla hoteles esta vacía");
+            }
+            else {
+                resultSet.close();
+                resultSet = statement.executeQuery(sqlQuery);
+                // Procesar los resultados de la consulta
+                System.out.println("Datos de la tabla hoteles:");
+                while (resultSet.next()) {//este comando pasa de fila hasta que no haya mas filasna
+                    String nombre = resultSet.getString("Nombre");
+                    Hoteles.add(nombre);
+                    System.out.println("  Nombre: " + nombre + " sacado de la base de datos");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Hoteles;
+    }
+}
